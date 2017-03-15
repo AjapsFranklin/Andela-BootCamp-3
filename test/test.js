956d99103686e2d16455159c3af682a24f6143c0
framky007 <ajapsfranklin@gmail.com>
2017-03-15 13:54:57 +0100

    Added Test Suites for Missing Number Function

--- test/test.js
+++ test/test.js
@@ -1,4 +1,4 @@
-  
+
 (function (){
 
     var myApp = require('../app/library2.js');
@@ -54,6 +54,7 @@ describe('Binary Search to traverse an ordered list, effectively', function() {
   });
   
   it('should not contain any form of `indexOf` in the search function', function() {
+    //var myIndexOf = new myApp.indexOf()
     expect(myApp.prototype.search.toString().indexOf('indexOf')).toBe(-1);      
   });
   
@@ -107,3 +108,70 @@ describe('Binary Search to traverse an ordered list, effectively', function() {
 
   });
 });
+
+
+
+ //FUNCTION FOR MISSING number
+describe("Find missing number two lists: ", function() {
+
+    describe("Case for invalid input", function() {
+
+    it("should return 'invalid input' for String in argument 1", function() {
+      expect(myApp.findMissing("frank", [])).toBe("invalid input");
+    });
+
+    it("should return 'invalid input' for String and number in argumnet 1 and 2", function() {
+      expect(myApp.findMissing("frank", 123)).toBe("invalid input");
+    });
+
+    it("should return 'invalid input' for 'null'", function() {
+      expect(myApp.findMissing(null, null)).toBe("invalid input");
+    });
+
+  });
+
+  describe("Case for ean empty list", function() {
+
+    it("should return 0 for empty list", function() {
+      expect(myApp.findMissing([], [])).toEqual(0);
+    });
+
+  });
+
+  describe("Case for lists with the same entries", function() {
+
+    it("should return 0 for [2],[2]", function() {
+      expect(myApp.findMissing([2], [2])).toEqual(0);
+    });
+
+    it("should return 0 for [4],[4]", function() {
+      expect(myApp.findMissing([4], [4])).toEqual(0);
+    });
+
+    it("should return 0 for [7],[7]", function() {
+      expect(myApp.findMissing([7], [7])).toEqual(0);
+    });
+
+  });
+
+  describe("Case for lists with similar entries and a missing number", function() {
+
+    it("should return 5 for [1,2],[1,2,5]", function() {
+      expect(myApp.findMissing([1, 2], [1, 2, 5])).toEqual(5);
+    });
+
+    it("should return 10 for [4, 6, 8],[4, 6, 8, 10]", function() {
+      expect(myApp.findMissing([4, 6, 8], [4, 6, 8, 10])).toEqual(10);
+    });
+
+    it("should return 1 for [5, 4, 7, 6, 11, 66],[5, 4, 1, 7, 6, 11, 66]", function() {
+      expect(myApp.findMissing([5, 4, 7, 6, 11, 66], [5, 4, 1, 7, 6, 11, 66])).toEqual(1);
+    });
+
+  });
+
+});
+})();
+    Array.prototype.indexOf = function () {
+  throw new Error('You are not allowed to use the indexOf function');
+};
